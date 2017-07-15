fcafeeaffcf57a77c35348c9897d486ac0985529
Ian "The_Cre8r" Harshbarger <TheCre8r@users.noreply.github.com>
2017-07-15 18:45:51 -0400

    Create WME-Page-Refresh.user.js

---
+++ WME-Page-Refresh.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         WME Page Refresh
+// @namespace    https://github.com/TheCre8r/WME-Page-Refresh
+// @version      1.0.0.0
+// @description  Allows Waze Editors to refresh the page without losing their spot.
+// @author       The_Cre8r
+// @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/.*$/
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    function disableF5(e) {
+        if ((e.which || e.keyCode) == 116 || (e.which || e.keyCode) == 82)
+            e.preventDefault();
+        if (event.defaultPrevented === true) {
+            var PERMALINK = document.getElementsByClassName("fa fa-link permalink")[0].href;
+            //alert("Was preventDefault() called: " + event.defaultPrevented + PERMALINK);
+            window.location.href = PERMALINK;
+            e.preventDefault();
+        }
+    }
+
+    $(document).ready(function() {
+        $(document).on("keydown", disableF5);
+        $(document).on("keydown", disableF5);
+    });
+
+
+})();
