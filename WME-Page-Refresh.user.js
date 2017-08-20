1fd99f0b68004ee68ab62977cf6388cbcef0a503
Ian "The_Cre8r" Harshbarger <TheCre8r@users.noreply.github.com>
2017-08-20 16:40:23 -0400

    Version 1.0.0.3
    - Updated to allow for new Waze URL
    - Fixed issue related to other keypresses forcing script to start
    - Added a marvelous pop-up for changes!

--- WME-Page-Refresh.user.js
+++ WME-Page-Refresh.user.js
@@ -1,30 +1,57 @@
 // ==UserScript==
 // @name         WME Page Refresh
 // @namespace    https://github.com/TheCre8r/WME-Page-Refresh
-// @version      1.0.0.2
+// @version      1.0.0.3
 // @description  Allows Waze Editors to refresh the page without losing their spot.
 // @author       The_Cre8r
-// @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
+// @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
 // @grant        none
 // ==/UserScript==
 (function() {
     'use strict';
 
+    // Update Pop Up
+    function updateAlert() {
+        var version = GM_info.script.version.toString();
+        var versionChanges = '';
+        var previousVersion;
+
+        versionChanges += 'WME Page Refresh v' + version + ' changes:\n';
+        versionChanges += ' - Updated to allow for new Waze URL \n - Fixed issue related to other keypresses forcing script to start\n - Added this marvelous pop-up!';
+
+        if (localStorage === void 0) {
+            return;
+        }
+
+        previousVersion = localStorage.WMEPageRefreshVersion;
+
+        if (version !== previousVersion) {
+            alert(versionChanges);
+            localStorage.WMEPageRefreshVersion = version;
+        }
+    }
+    $(document).ready(function() {
+        console.log( "WME Page Refresh Intialized" );
+        updateAlert();
+    });
+    //  Function
     function disableF5(e) {
         if ((e.which || e.keyCode) == 116)
             e.preventDefault();
         if (event.defaultPrevented === true) {
-            var PERMALINK = document.getElementsByClassName("fa fa-link permalink")[0].href;
+            if (e.keyCode != 116)
+            {
+            }
+            else
+            {
+                var PERMALINK = document.getElementsByClassName("fa fa-link permalink")[0].href;
             //alert("Was preventDefault() called: " + event.defaultPrevented + PERMALINK);
             window.location.href = PERMALINK;
             e.preventDefault();
+            }
         }
     }
 
-    $(document).ready(function() {
-        $(document).on("keydown", disableF5);
-        $(document).on("keydown", disableF5);
-    });
-
+    $(document).ready(function() {$(document).on("keydown", disableF5);});
 
 })();
