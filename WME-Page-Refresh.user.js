98acbd2186fe0eb95b5d01af94f0c1beff651d9d
Ian "The_Cre8r" Harshbarger <TheCre8r@users.noreply.github.com>
2017-10-29 15:07:16 -0400

    Version 1.0.0.4
    Updated to work with new WME layout and added a secret setting to override F5

--- WME-Page-Refresh.user.js
+++ WME-Page-Refresh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Page Refresh
 // @namespace    https://github.com/TheCre8r/WME-Page-Refresh
-// @version      1.0.0.3
+// @version      1.0.0.4
 // @description  Allows Waze Editors to refresh the page without losing their spot.
 // @author       The_Cre8r
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
@@ -11,13 +11,13 @@
     'use strict';
 
     // Update Pop Up
-    function updateAlert() {
+    function onStart() {
         var version = GM_info.script.version.toString();
         var versionChanges = '';
         var previousVersion;
 
         versionChanges += 'WME Page Refresh v' + version + ' changes:\n';
-        versionChanges += ' - Updated to allow for new Waze URL \n - Fixed issue related to other keypresses forcing script to start\n - Added this marvelous pop-up!';
+        versionChanges += ' - Updated to work with new WME layout\n - Added a secret setting to override F5';
 
         if (localStorage === void 0) {
             return;
@@ -29,22 +29,27 @@
             alert(versionChanges);
             localStorage.WMEPageRefreshVersion = version;
         }
+        if (localStorage.WMEPageRefreshKeyCode === undefined)
+        {
+            localStorage.setItem("WMEPageRefreshKeyCode", 116);
+        }
+
     }
     $(document).ready(function() {
         console.log( "WME Page Refresh Intialized" );
-        updateAlert();
+        onStart();
     });
     //  Function
     function disableF5(e) {
-        if ((e.which || e.keyCode) == 116)
+        if ((e.which || e.keyCode) == localStorage.WMEPageRefreshKeyCode)
             e.preventDefault();
         if (event.defaultPrevented === true) {
-            if (e.keyCode != 116)
+            if (e.keyCode != localStorage.WMEPageRefreshKeyCode)
             {
             }
             else
             {
-                var PERMALINK = document.getElementsByClassName("fa fa-link permalink")[0].href;
+                var PERMALINK = document.getElementsByClassName("permalink")[0].href;
             //alert("Was preventDefault() called: " + event.defaultPrevented + PERMALINK);
             window.location.href = PERMALINK;
             e.preventDefault();
